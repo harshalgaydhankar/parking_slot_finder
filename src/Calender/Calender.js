8ab8639da50c2c428a6250569a0b9abcecf653d0
harshalgaydhankar <harshal.gaydhankar@axisbank.com>
2021-07-25 18:08:05 +0530

    wiring up slots in calender

--- src/Calender/Calender.js
+++ src/Calender/Calender.js
@@ -1,8 +1,18 @@
+const Slots = require("./Slots/Slots");
 
 class Calender {
     constructor(startTime, endTime) {
         this.startTime = startTime;
         this.endTime = endTime;
+        this.slots = new Slots(startTime, endTime);
+    }
+
+    getAvailableSlots() {
+        return this.slots.getAvailableSlots();
+    }
+
+    book(timeSlotsToBeBooked) {
+        return this.slots.book(timeSlotsToBeBooked);
     }
 }
 
--- src/Calender/Calender.test.js
+++ src/Calender/Calender.test.js
@@ -3,8 +3,41 @@ const Calender = require("./Calender");
 describe('Calender', () => {
 
     it('should have startTime and endTime Property which denotes working slots in calender', () => {
-        const calender = new Calender( 10, 18);
+        const calender = new Calender( 10, 11);
         expect(calender.startTime).toEqual(10);
-        expect(calender.endTime).toEqual(18);
+        expect(calender.endTime).toEqual(11);
+        expect(calender.slots.length).toEqual(12);
+    });
+
+    it('should return list of available slots', () => {
+        const calender = new Calender( 10, 11);
+        const availableSlots = calender.getAvailableSlots();
+        expect(availableSlots[0].from).toEqual("10.00");
+        expect(availableSlots[0].to).toEqual("11.00");
+    });
+
+
+    it('should book slots with given timeslots', () => {
+        const timeSlotsToBeBooked = [
+            {
+                from: 10.15,
+                to: 10.30
+            },
+            {
+                from: 10.40,
+                to: 10.50
+            },
+
+        ];
+        const expectedAvailableSlots = [
+            {from: '10.00', to: '10.15'},
+            {from: '10.30', to: '10.40'},
+            {from: '10.50', to: '11.00'}
+        ];
+        const calender = new Calender( 10, 11);
+
+        calender.book(timeSlotsToBeBooked);
+        const availableSlots = calender.getAvailableSlots();
+        expect(availableSlots).toEqual(expectedAvailableSlots);
     });
 });
