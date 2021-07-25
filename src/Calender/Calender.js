44b9b9c23e79b134d4c5dff8d5080fd987d62762
harshalgaydhankar <harshal.gaydhankar@axisbank.com>
2021-07-25 14:19:27 +0530

    Calendershould have startTime and endTime Property which denotes working slots in calender

---
+++ src/Calender/Calender.js
@@ -0,0 +1,10 @@
+
+class Calender {
+    constructor(interval, startTime, endTime) {
+        this.interval = interval;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+}
+
+module.exports = Calender;
--- src/Calender/Calender.test.js
+++ src/Calender/Calender.test.js
@@ -1,9 +1,16 @@
-
+const Calender = require("./Calender");
 
 describe('Calender', () => {
 
     it('should have interval property which states calender interval', () => {
-        Calender calender =- new Calender(5);
+        const calender = new Calender(5);
         expect(calender.interval).toEqual(5);
+    });
+
+    it('should have startTime and endTime Property which denotes working slots in calender', () => {
+        const calender = new Calender(5, 10, 6);
+        expect(calender.interval).toEqual(5);
+        expect(calender.startTime).toEqual(5);
+        expect(calender.endTime).toEqual(5);
     });
 });
