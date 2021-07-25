917295838f474c332fca574cb5dd2c63b58a526d
harshalgaydhankar <harshal.gaydhankar@axisbank.com>
2021-07-25 18:05:11 +0530

    add book slot feature

--- src/Calender/Slots/Slots.js
+++ src/Calender/Slots/Slots.js
@@ -1,5 +1,6 @@
 const Slot = require("./Slot");
 const {interval} = require("../../constants");
+const {adjustTimeToNextSlotTime} = require("../../utils");
 
 class Slots extends Array {
     constructor(startTime = 10, endTime = 18) {
@@ -15,16 +16,48 @@ class Slots extends Array {
         }
     }
 
-    getAvailableSlots(startTime, endTime) {
-        let endIndex = this.findIndex(slot => slot.time == endTime);
-        if(endIndex === -1){
-            return [
-                {
-                    from : startTime,
-                    to: endTime
+    getAvailableSlots() {
+        const availableChunks = [];
+        const availableSlots = [];
+        for (let i = 0; i < this.length; i++) {
+            if (this[i].isAvailable) {
+                availableChunks.push(this[i])
+            } else {
+                if(availableChunks.length > 0) {
+                    availableSlots.push({
+                        from: availableChunks[0].time,
+                        to: adjustTimeToNextSlotTime(availableChunks[availableChunks.length - 1].time)
+                    });
                 }
-            ]
+                availableChunks.splice(0, availableChunks.length)
+            }
         }
+        if (availableChunks.length === this.length) {
+            availableSlots.push({
+                from: this[0].time,
+                to: adjustTimeToNextSlotTime(this[this.length - 1].time)
+            });
+        }
+        else {
+            availableSlots.push({
+                from: availableChunks[0].time,
+                to: adjustTimeToNextSlotTime(availableChunks[availableChunks.length - 1].time)
+            });
+        }
+        return availableSlots;
+
+    }
+
+    book(timeSlotsToBeBooked) {
+        let startIndex, endIndex;
+        timeSlotsToBeBooked.forEach((timeSlot) => {
+            startIndex = this.findIndex(slot => slot.time == timeSlot.from);
+            endIndex = this.findIndex(slot => slot.time == timeSlot.to);
+            console.log(startIndex + " " + endIndex);
+            for (let i = startIndex; i < endIndex; i++) {
+                this[i].book();
+            }
+        });
     }
 }
 
--- src/Calender/Slots/Slots.test.js
+++ src/Calender/Slots/Slots.test.js
@@ -13,10 +13,34 @@ describe('Slots', () => {
     });
 
     it('should return list of available slots with startTime and endTime', () => {
-        const slots = new Slots(10, 18);
-        const availableSlots = slots.getAvailableSlots(10, 18);
-        expect(availableSlots[0].from).toEqual(10);
-        expect(availableSlots[0].to).toEqual(18);
+        const slots = new Slots(10, 11);
+        const availableSlots = slots.getAvailableSlots();
+        expect(availableSlots[0].from).toEqual("10.00");
+        expect(availableSlots[0].to).toEqual("11.00");
+    });
+
+    it('should book slots with given timeslots', () => {
+        const timeSlotsToBeBooked = [
+            {
+                from: 10.15,
+                to: 10.30
+            },
+            {
+                from: 10.40,
+                to: 10.50
+            },
+
+        ];
+        const expectedAvailableSlots = [
+            {from: '10.00', to: '10.15'},
+            {from: '10.30', to: '10.40'},
+            {from: '10.50', to: '11.00'}
+        ];
+        const slots = new Slots(10, 11);
+        expect(slots.length).toEqual(12);
+        slots.book(timeSlotsToBeBooked);
+        const availableSlots = slots.getAvailableSlots();
+        expect(availableSlots).toEqual(expectedAvailableSlots);
     });
 
 });
