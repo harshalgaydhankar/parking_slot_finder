52c3a656c5221602b9fc62a20681144e7e8a86e0
harshalgaydhankar <harshal.gaydhankar@axisbank.com>
2021-07-25 15:33:49 +0530

    initialise slots with given starttime and endtime with interval

--- src/Calender/Slots/Slots.js
+++ src/Calender/Slots/Slots.js
@@ -1,8 +1,19 @@
+const Slot = require("./Slot");
 
 class Slots extends Array {
-    constructor() {
+    constructor(startTime = 10, endTime = 18, interval = 0.05) {
         super();
+        this.initialiseSlots(startTime, endTime, interval);
     }
+
+    initialiseSlots(startTime, endTime, interval) {
+        for (let i = startTime; i<endTime; i +=interval ){
+            if(i - parseInt(i) < 0.60) {
+                this.push(new Slot(i));
+            }
+        }
+    }
+
 }
 
 module.exports = Slots;
--- src/Calender/Slots/Slots.test.js
+++ src/Calender/Slots/Slots.test.js
@@ -6,4 +6,10 @@ describe('Slots', () => {
         const slots = new Slots();
         expect(slots instanceof Array).toEqual(true);
     });
+
+    it('should initialise slots with default startTime and endTime with difference of default interval 5 mins', () => {
+        const slots = new Slots();
+        expect(slots.length).toEqual(96);
+    });
+
 });
